5e44345cfa30b13a9582ae364bdbf459addadb9c
Alex-Nagy <nagyal3x@gmail.com>
2022-02-02 11:02:30 +0100

    delete post works

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,49 @@ import "./App.css";
 
 function App() {
   const [blogPosts, setBlogPosts] = useState([]);
-  
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
   const postBlogPost = () => {
     setBlogPosts([
       ...blogPosts,
-      { title: "Cím1", content: "Tartalom 1", date: "Dátum 1" },
+      { title: title, content: content, date: new Date().toString()},
     ]);
+    setTitle("");
+    setContent("");
+  };
+
+  const deleteBlogPosts = () => {
+    setBlogPosts([]);
+  };
+
+  const removeBlogPost = (uid) => {
+
+    /*     
+    for (const blog of blogPosts) {
+      if(blog.date !== uid){
+        list.push(blog)
+      }
+    } 
+    */
+
+    setBlogPosts(blogPosts.filter((b) => b.date !== uid));
   };
 
   return (
     <main className="App">
-      <input type="text" placeholder="Title" />
-      <input type="text" placeholder="Content" />
+      <input
+        type="text"
+        placeholder="Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Content"
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+      />
       <button onClick={postBlogPost}>Post</button>
       <h1>Posts</h1>
       {blogPosts.map((blogPost, i) => (
@@ -27,9 +58,10 @@ function App() {
           <button>Edit</button>
           <button>Save</button>
           <button>Cancel</button>
-          <button>Remove</button>
+          <button onClick={() => removeBlogPost(blogPost.date)}>Remove</button>
         </article>
       ))}
+      <button onClick={deleteBlogPosts}>Delete All</button>
     </main>
   );
 }
