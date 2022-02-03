c93e270d98455ba538e9e6e22bd4307863cabc32
Alex-Nagy <nagyal3x@gmail.com>
2022-02-03 12:52:36 +0100

    editBlogPost

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,43 @@
 import { useState } from "react";
 import "./App.css";
 
+const BlogPost = ({ blogPost, editBlogPost, removeBlogPost }) => {
+  const [editTitle, setEditTitle] = useState("");
+  const [editText, setEditText] = useState("");
+
+  return (
+    <article>
+      <h2>{blogPost.title}</h2>
+      <p>{blogPost.content}</p>
+      <h5>{blogPost.date}</h5>
+      <input
+        type="text"
+        placeholder="Title"
+        value={editTitle}
+        onChange={(e) => setEditTitle(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Content"
+        value={editText}
+        onChange={(e) => setEditText(e.target.value)}
+      />
+      <button
+        onClick={() => {
+          editBlogPost(blogPost.date, editTitle, editText);
+          setEditTitle("");
+          setEditText("");
+        }}
+      >
+        Edit
+      </button>
+      <button>Save</button>
+      <button>Cancel</button>
+      <button onClick={() => removeBlogPost(blogPost.date)}>Remove</button>
+    </article>
+  );
+};
+
 function App() {
   const [blogPosts, setBlogPosts] = useState([]);
   const [title, setTitle] = useState("");
@@ -9,10 +46,29 @@ function App() {
   const postBlogPost = () => {
     setBlogPosts([
       ...blogPosts,
-      { title: title, content: content, date: new Date().toString().substring(0, 25)},
+      {
+        title: title,
+        content: content,
+        date: new Date().toString().substring(0, 25),
+      },
     ]);
     setTitle("");
     setContent("");
+  };
+
+  const editBlogPost = (uid, editTitle, editText) => {
+    console.log(uid);
+    const currenList = blogPosts;
+    const nextList = [];
+    // todo
+    for (const post of currenList) {
+      if (post.date !== uid) {
+        nextList.push(post);
+      } else {
+        nextList.push({ title: editTitle, content: editText, date: post.date });
+      }
+    }
+    setBlogPosts(nextList);
   };
 
   const deleteBlogPosts = () => {
@@ -20,7 +76,6 @@ function App() {
   };
 
   const removeBlogPost = (uid) => {
-
     /*     
     for (const blog of blogPosts) {
       if(blog.date !== uid){
@@ -49,19 +104,16 @@ function App() {
       <button onClick={postBlogPost}>Post</button>
       <h1>Posts</h1>
       {blogPosts.map((blogPost, i) => (
-        <article key={i}>
-          <h2>{blogPost.title}</h2>
-          <p>{blogPost.content}</p>
-          <h5>{blogPost.date}</h5>
-          <input type="text" placeholder="Title" />
-          <input type="text" placeholder="Content" />
-          <button>Edit</button>
-          <button>Save</button>
-          <button>Cancel</button>
-          <button onClick={() => removeBlogPost(blogPost.date)}>Remove</button>
-        </article>
+        <BlogPost
+          key={i}
+          blogPost={blogPost}
+          editBlogPost={editBlogPost}
+          removeBlogPost={removeBlogPost}
+        />
       ))}
-      <button onClick={deleteBlogPosts} className="del-btn">Delete All</button>
+      <button onClick={deleteBlogPosts} className="del-btn">
+        Delete All
+      </button>
     </main>
   );
 }
